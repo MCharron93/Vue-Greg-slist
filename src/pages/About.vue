1b18ad01baa78ab4b279a88415fe8a9d74c0f077
Michelle Charron <michelle.charron93@gmail.com>
2020-11-04 14:06:18 -0700

    Initial page set up and doc create in vue

---
+++ src/pages/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Welcome to Greg'sList</h1>
+    <p>Here you can find all sorts of items and fun gadgets</p>
+  </div>
+</template>
---
+++ src/pages/CarsPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="div container-fluid">
+    Hello from Cars Page;
+    When we hit this page, we want to make a call to the api for cars
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CarsPage'
+
+}
+</script>
+
+<style>
+
+</style>
